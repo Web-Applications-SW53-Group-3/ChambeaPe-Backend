6caa5f8a719f5d61887e81dd4510e76ca6d10c8d
Jenn Espinoza <jenniespinoza2002@hotmail.com>
2023-10-31 20:58:46 -0500

    feat: add tests for userdomain

---
+++ 2. Domain.Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace _2._Domain.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ 2. Domain.Tests/WorkerTest/WokerDomainTest.cs
@@ -0,0 +1,19 @@
+using _2._Domain.Users;
+using _2._Domain.Workers;
+using _3._Data.Model;
+using _3._Data.Users;
+using _3._Data.Workers;
+using NSubstitute;
+
+namespace _2._Domain.Tests.WorkerTest;
+
+public class WokerDomainTest
+{
+    //Todo: happy path CreateAsync 
+    [Fact]
+public async Task CreateAsync_ValidData_ResultTrue()
+    {
+        
+    } 
+    
+}

64431b4c4f7c09dc70c6d274717ccf2c4b94a30c
Diego Castro <diego_castro_soto@hotmail.com>
2023-11-11 17:39:21 -0500

    feat: add user authentication controller and login request

---
+++ 1. API/Request/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace _1._API.Request
+{
+    public class LoginRequest
+    {
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}
